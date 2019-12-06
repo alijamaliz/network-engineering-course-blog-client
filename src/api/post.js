7c0742af062fa196347dc37ce4a580b93143b624
Ali <jamaliali219@gmail.com>
2019-12-06 23:29:47 +0330

    Implement like post request

--- src/api/post.js
+++ src/api/post.js
@@ -3,7 +3,22 @@ import { API_BASE_URL } from '../config/constants';
 export const getAllPosts = async () => {
     return new Promise((resolve, reject) => {
         fetch(`${API_BASE_URL}/posts/`)
-            .then(res => res.json())
+            .then(res => {
+                return res.json();
+            })
+            .then(
+                result => resolve(result),
+                error => reject(error)
+            );
+    });
+};
+
+export const likePost = async postId => {
+    return new Promise((resolve, reject) => {
+        fetch(`${API_BASE_URL}/posts/${postId}/like/`, { method: 'post' })
+            .then(res => {
+                return res.json();
+            })
             .then(
                 result => resolve(result),
                 error => reject(error)
